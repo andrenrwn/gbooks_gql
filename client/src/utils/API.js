1363c645045342ff298588953546292a183be42c
anrwn <andrenrwn@users.noreply.github.com>
2024-01-12 22:56:42 +0800

    Populate Book link property from Google Book API's volumeInfo.infoLink

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -42,15 +42,16 @@
 //   });
 // };
 
-// remove saved book data for a logged in user
-export const deleteBook = (bookId, token) => {
-  return fetch(`/api/users/books/${bookId}`, {
-    method: 'DELETE',
-    headers: {
-      authorization: `Bearer ${token}`,
-    },
-  });
-};
+// Deprecated
+// // remove saved book data for a logged in user
+// export const deleteBook = (bookId, token) => {
+//   return fetch(`/api/users/books/${bookId}`, {
+//     method: 'DELETE',
+//     headers: {
+//       authorization: `Bearer ${token}`,
+//     },
+//   });
+// };
 
 // make a search to google books api
 // https://www.googleapis.com/books/v1/volumes?q=harry+potter
